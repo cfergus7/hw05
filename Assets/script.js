225aa921db13bcc2fd263a5ef2ac03916d32d412
cfergus7 <64560968+cfergus7@users.noreply.github.com>
2020-06-02 01:09:12 -0500

    Update script.js

--- Assets/script.js
+++ Assets/script.js
@@ -1,11 +1,14 @@
+$(document).ready(function(){
 //----define moment varibles used to show time----//
+function updateTime(){
 var date = moment().format('MMMM Do YYYY, h:mm:ss a');
-// var currentTime = moment().format('H');
-// console.log(currentTime);
 
-//-------append date----------//
-$("#currentDay").append(date);
-
+$("#currentDay").html(date);
+};
+updateTime();
+setInterval(function(){
+    updateTime();
+},1000);
 
 //---------background Updater------------------------//
 
